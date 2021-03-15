98e9b309828b1fc3e8db28e528b29142a77ef621
Jackson Beecher <jbeecher@ashcom.com.au>
2021-03-15 17:13:49 +1000

    Lab 1 and 2

---
+++ 2701ICT/lab1/lab1_js/main2.js
@@ -0,0 +1,6 @@
+var no1 = 20;
+var no2 = 0.5;
+var no3 = 0.5;
+var sum = no1 + no2 + no3;
+var text = "Sum of numbers is: " + sum.toString();
+console.log(text);
---
+++ 2701ICT/lab1/lab1_js/main2.ts
@@ -0,0 +1,6 @@
+var no1: number = 20;
+var no2: number = 0.5;
+var no3: number = 0.5;
+let sum: number = no1 + no2 + no3;
+let text: string = "Sum of numbers is: " + sum
+console.log(text);
