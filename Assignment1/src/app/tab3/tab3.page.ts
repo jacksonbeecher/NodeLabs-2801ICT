a557b79d75adbb165bc6cb5d2a6f2bb618a0d0bd
Jackson Beecher <jbeecher@ashcom.com.au>
2021-05-10 18:26:28 +1000

    Forgot to push

--- Assignment1/src/app/tab3/tab3.page.ts
+++ Assignment1/src/app/tab3/tab3.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  user:String;
+  date:Date;
+  title:String;
+  location:String;
 
+  friends =[{user: 'Aaron', date:'2021/5/5', title: "Legs", location:'The local.'},
+            {user: 'Dave', date:'2021/5/6', title: "Upper Body", location:'The local.'}]
   constructor() {}
 
 }
