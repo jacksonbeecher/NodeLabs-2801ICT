3c7a7749098e5eaeeebdf525da6695bc561e0768
Jackson Beecher <jbeecher@ashcom.com.au>
2021-05-24 16:55:34 +1000

    Revert "Forgot to push"
    This reverts commit a557b79d75adbb165bc6cb5d2a6f2bb618a0d0bd.

--- Assignment1/src/app/tabs/tabs-routing.module.ts
+++ Assignment1/src/app/tabs/tabs-routing.module.ts
@@ -20,10 +20,6 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-        path: 'account/:username',
-        loadChildren: () => import('../account/account.module').then(m => m.AccountPageModule)
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
