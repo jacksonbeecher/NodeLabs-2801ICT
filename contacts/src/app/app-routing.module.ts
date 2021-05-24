65ed24f3f627b7a30602503328f43f6b253f381b
Jackson Beecher <jbeecher@ashcom.com.au>
2021-05-25 00:02:01 +1000

    Create Map with marker. Workshop 9.

--- contacts/src/app/app-routing.module.ts
+++ contacts/src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
   {
     path: 'modal',
     loadChildren: () => import('./modal/modal.module').then( m => m.ModalPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   }
   
 ];
---
+++ contacts/src/app/tab4/tab4.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare let google;
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+})
+
+
+export class Tab4Page implements OnInit {
+  @ViewChild('map',{static:true}) mapElement:ElementRef;
+  map:any;
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ngOnInit Tab4Page');
+    let latLng = new google.maps.LatLng(27.5530, 153.0512);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement , mapOptions);
+    let marker = new google.maps.Marker({
+      map:this.map,
+      animation: google.maps.Animation.Drop,
+      position: this.map.getCenter()
+    })
+    let infoWindow = new google.maps.InfoWindow({
+      content: '<h4>Griffith</h4>'
+      });
+      google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+      });
+  }
+  
+}
